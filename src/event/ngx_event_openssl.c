6de0b9b32cf64e6229f2f72c953a08c86ea8c84c
Jairus Christensen <christensenjairus@gmail.com>
2022-11-21 21:21:35 -0700

    copy str, not str pointer

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -4930,8 +4930,8 @@ ngx_int_t
 ngx_ssl_get_rtt(ngx_connection_t *c, ngx_pool_t *pool, ngx_str_t *s)
 {
     char str[256]; // create string
-    sprintf(str, "%lli", SSL_get_rtt(c->ssl->session)); // copy to string
-    s->data = (u_char *) str; // copy to string data.
+    sprintf(str, "%lli", SSL_get_rtt(c->ssl->session)); // copy long long to string
+    s->data = (u_char *) str&; // copy to string data.
     return NGX_OK;
 }
 
