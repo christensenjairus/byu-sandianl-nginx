50dfe50c3d029742550269cdf296001f47cca55a
jairusc <jairusc@qualtrics.com>
2022-11-22 09:53:57 -0700

    added log file for if ssl_get_rtt() fails

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -4932,6 +4932,13 @@ ngx_ssl_get_rtt(ngx_connection_t *c, ngx_pool_t *pool, ngx_str_t *s)
     uint64_t *tmp_rtt = malloc(sizeof(uint64_t)); // create mem space to pass to openssl
     int success = SSL_get_rtt(c->ssl->session, tmp_rtt); // openssl fills in the mem space
     if (success != 1) {
+        FILE* rttlogfile = fopen("/tmp/nginx_rtt.log", "a");
+        if(rttlogfile==NULL) perror("Can't open rtt log file");
+        else {
+            fprintf(rttlogfile, "SSL_get_rtt() failed");
+            fclose(rttlogfile);
+        }
+
         return NGX_ERROR;
     }
     memcpy(s->data, tmp_rtt, sizeof(uint64_t));  // copy the data from memory into variable's data
