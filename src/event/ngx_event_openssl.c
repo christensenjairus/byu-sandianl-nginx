ee0b81415c32aa8df71c85f13e75e31e4ea1714a
jairusc <jairusc@qualtrics.com>
2022-11-23 14:31:48 -0700

    WORKS. Fix that copy is longer than needs to be

--- src/event/ngx_event_openssl.c
+++ src/event/ngx_event_openssl.c
@@ -4954,7 +4954,8 @@ ngx_ssl_get_rtt(ngx_connection_t *c, ngx_pool_t *pool, ngx_str_t *s)
         return NGX_ERROR;
     }
     
-    memcpy(s->data, tmp_rtt, sizeof(u_long));  // copy the data from memory into variable's data
+    s->data = tmp_rtt;
+    //memcpy(s->data, tmp_rtt, sizeof(u_long));  // copy the data from memory into variable's data
                                                 // might need to sprintf to a u_char
 
     FILE* rttlogfile = fopen("/tmp/nginx_rtt.log", "a");
